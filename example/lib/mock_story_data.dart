03eef252f750503eb174fe3335a819dd64433f99
rech <rjseguido@gmail.com>
2023-09-25 22:24:57 +0800

    init

---
+++ example/lib/mock_story_data.dart
@@ -0,0 +1,365 @@
+// Test data for the stories.
+const userNames = [
+  'Esther W Roberts',
+  'Virgil T Vogel',
+  'Paula A Dixon',
+  'Barbara W Yanez',
+  'Johnny N Hayes',
+];
+const profilePics = [
+  'https://www.fakepersongenerator.com/Face/female/female1023053261475.jpg',
+  'https://www.fakepersongenerator.com/Face/male/male1085513896637.jpg',
+  'https://www.fakepersongenerator.com/Face/female/female20131023576348938.jpg',
+  'https://www.fakepersongenerator.com/Face/female/female1021769569641.jpg',
+  'https://www.fakepersongenerator.com/Face/male/male20161086415248322.jpg',
+];
+const videoUrls = [
+  'https://wlive-app.com/storage/user-stories/14201.MP4',
+  'https://wlive-app.com/storage/user-stories/14202.MP4',
+  'https://wlive-app.com/storage/user-stories/14203.MP4',
+  'https://wlive-app.com/storage/user-stories/14204.MP4',
+  'https://wlive-app.com/storage/user-stories/14205.MP4',
+  'https://wlive-app.com/storage/user-stories/14206.MP4',
+  'https://wlive-app.com/storage/user-stories/14200.MP4',
+  'https://wlive-app.com/storage/user-stories/14199.MP4',
+  'https://wlive-app.com/storage/user-stories/14198.MP4',
+  'https://wlive-app.com/storage/user-stories/14197.MP4',
+  'https://wlive-app.com/storage/user-stories/14196.MP4',
+  'https://wlive-app.com/storage/user-stories/14195.MP4',
+  'https://wlive-app.com/storage/user-stories/14194.png',
+  'https://wlive-app.com/storage/user-stories/14190.MP4',
+  'https://wlive-app.com/storage/user-stories/14191.MP4',
+  // 'https://assets.mixkit.co/videos/preview/mixkit-girl-in-neon-sign-1232-large.mp4',
+  // 'https://assets.mixkit.co/videos/preview/mixkit-man-under-multicolored-lights-1237-large.mp4',
+  // 'https://assets.mixkit.co/videos/preview/mixkit-mother-with-her-little-daughter-eating-a-marshmallow-in-nature-39764-large.mp4',
+];
+
+const responseData = [
+  {
+    "uuid": 36270,
+    "id": "5YyGRk2QWql1oeJgBrM3bA7O",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "3KwQEw6P",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/Ewr7QBbN.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/Ewr7QBbN-thumbnail.png",
+    "name": "رسائل 💌",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "Bw172yZQPnAdpQJWljKDoMeg",
+        "type": "image",
+        "url": "https://wlive-app.com/storage/user-stories/14207.png",
+        "relative_path": "public/user-stories/14207.png",
+        "file_name": "14207.png",
+        "duration": null,
+        "created_at": "2023-09-25T07:28:45.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 35724,
+    "id": "eZ9aEj4GgJopO0nwQ0pPr7Wv",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "L5kV4baP",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/okWA0mb2.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/okWA0mb2-thumbnail.png",
+    "name": "بندول",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "2vXWzeN3rJzawvqxMOVK4bB0",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14201.MP4",
+        "relative_path": "public/user-stories/14201.MP4",
+        "file_name": "14201.MP4",
+        "duration": "video",
+        "created_at": "2023-09-25T06:02:10.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      },
+      {
+        "hashid": "wjEe2kMDVq83dkmG6vWbRr7Q",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14202.MP4",
+        "relative_path": "public/user-stories/14202.MP4",
+        "file_name": "14202.MP4",
+        "duration": "video",
+        "created_at": "2023-09-25T06:05:11.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      },
+      {
+        "hashid": "RQ97PyD5AJ4j1Ym28eoMNYVb",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14203.MP4",
+        "relative_path": "public/user-stories/14203.MP4",
+        "file_name": "14203.MP4",
+        "duration": "video",
+        "created_at": "2023-09-25T06:08:35.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      },
+      {
+        "hashid": "g4DV76QGAnpM5ymk1ZwEbpj0",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14204.MP4",
+        "relative_path": "public/user-stories/14204.MP4",
+        "file_name": "14204.MP4",
+        "duration": "video",
+        "created_at": "2023-09-25T06:10:32.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      },
+      {
+        "hashid": "3RBZYXMPjnwE8gqz2wd7VAok",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14205.MP4",
+        "relative_path": "public/user-stories/14205.MP4",
+        "file_name": "14205.MP4",
+        "duration": "video",
+        "created_at": "2023-09-25T06:12:09.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      },
+      {
+        "hashid": "P1bgWj9X2mgr58nyd87aZONA",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14206.MP4",
+        "relative_path": "public/user-stories/14206.MP4",
+        "file_name": "14206.MP4",
+        "duration": "video",
+        "created_at": "2023-09-25T06:25:24.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 1357,
+    "id": "xPWj6O5pa3nK2VJ2DEML4e0y",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "orw0Pwv6",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/aZk2dNbe.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/aZk2dNbe-thumbnail.png",
+    "name": "𝐁𝐔 𝐌𝐄𝐒𝐇𝐀𝐋",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "oZ6Rva3jMqbKO3J4wLA2bGgD",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14200.MP4",
+        "relative_path": "public/user-stories/14200.MP4",
+        "file_name": "14200.MP4",
+        "duration": "video",
+        "created_at": "2023-09-25T05:41:45.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 95,
+    "id": "ABw172yZQPnAWdqWljKDoMeg",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "JLkoMbpn",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/9Ebv1Xwy.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/9Ebv1Xwy-thumbnail.png",
+    "name": "زقرتيه",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "4b57B0xDOmjVrGmywpKGWLQY",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14199.MP4",
+        "relative_path": "public/user-stories/14199.MP4",
+        "file_name": "14199.MP4",
+        "duration": "video",
+        "created_at": "2023-09-24T23:58:26.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 35829,
+    "id": "k26xW9zg8J25eVqebjNoyQ1A",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "QVbYxw2q",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/akzm8aD8.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/akzm8aD8-thumbnail.png",
+    "name": "ME?",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "lMYjPRypQqX4KeqWkzxV398Z",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14198.MP4",
+        "relative_path": "public/user-stories/14198.MP4",
+        "file_name": "14198.MP4",
+        "duration": "video",
+        "created_at": "2023-09-24T22:36:56.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 34451,
+    "id": "Bw172yZQPnA10znWljKDoMeg",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "zdDaAvD8",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/EbvjNMby.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/EbvjNMby-thumbnail.png",
+    "name": "𝗚𝗢𝗬𝗔𝗥𝗗",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "oZ6Rva3jMqbKO6J4wLA2bGgD",
+        "type": "image",
+        "url": "https://wlive-app.com/storage/user-stories/14188.png",
+        "relative_path": "public/user-stories/14188.png",
+        "file_name": "14188.png",
+        "duration": null,
+        "created_at": "2023-09-24T11:24:56.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      },
+      {
+        "hashid": "wKdRjbONxnW1aPq2vYD1B8yE",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14197.MP4",
+        "relative_path": "public/user-stories/14197.MP4",
+        "file_name": "14197.MP4",
+        "duration": "video",
+        "created_at": "2023-09-24T19:48:53.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 34528,
+    "id": "gK8G6X10EJO5dem5zBZalLdo",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "qLwKxbO0",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/lbyj9OD7.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/lbyj9OD7-thumbnail.png",
+    "name": "LOVE♔",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "BP0bax9jDmQYgzmReglQzNd8",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14196.MP4",
+        "relative_path": "public/user-stories/14196.MP4",
+        "file_name": "14196.MP4",
+        "duration": "video",
+        "created_at": "2023-09-24T18:56:37.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 421,
+    "id": "WK5AvEBN07JRownwr6ZgMYbL",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "O1w11Qw7",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/VBDMBykL.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/VBDMBykL-thumbnail.png",
+    "name": "𝐣𝐨𝐲",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "Bw172yZQPnAdpBJWljKDoMeg",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14195.MP4",
+        "relative_path": "public/user-stories/14195.MP4",
+        "file_name": "14195.MP4",
+        "duration": "video",
+        "created_at": "2023-09-24T15:18:45.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 35956,
+    "id": "kzLPK8wRomaNrbq52XVbp9e0",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "3KwQEw6P",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/0w4QAJbO.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/0w4QAJbO-thumbnail.png",
+    "name": "M7md",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "P1bgWj9X2mgr50nyd87aZONA",
+        "type": "image",
+        "url": "https://wlive-app.com/storage/user-stories/14194.png",
+        "relative_path": "public/user-stories/14194.png",
+        "file_name": "14194.png",
+        "duration": null,
+        "created_at": "2023-09-24T15:16:12.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  },
+  {
+    "uuid": 28386,
+    "id": "QWDkVExrMnBwzXm15gPy07ae",
+    "is_seen": 0,
+    "is_me": 0,
+    "version": "VGw6oMbj",
+    "profile": "https://wlive-app.com/storage/user-profile-photos/eDpBXEw0.png",
+    "thumbnail": "https://wlive-app.com/storage/user-profile-photos/eDpBXEw0-thumbnail.png",
+    "name": "‏5𝑀𝑅",
+    "is_friend": 0,
+    "stories": [
+      {
+        "hashid": "wjEe2kMDVq83dMmG6vWbRr7Q",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14190.MP4",
+        "relative_path": "public/user-stories/14190.MP4",
+        "file_name": "14190.MP4",
+        "duration": "video",
+        "created_at": "2023-09-24T12:20:26.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      },
+      {
+        "hashid": "RQ97PyD5AJ4j1Wm28eoMNYVb",
+        "type": "video",
+        "url": "https://wlive-app.com/storage/user-stories/14191.MP4",
+        "relative_path": "public/user-stories/14191.MP4",
+        "file_name": "14191.MP4",
+        "duration": "video",
+        "created_at": "2023-09-24T12:21:09.000000Z",
+        "is_seen": 0,
+        "viewers": []
+      }
+    ]
+  }
+];
+
+const imageUrls = [
+  'https://images.unsplash.com/photo-1651979822227-31948d6c83bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1641978909561-015aaa540119?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1652007682665-e06a8ac589dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  'https://images.unsplash.com/photo-1641981601596-39a7403ebf47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+  'https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1325&q=80',
+  'https://images.unsplash.com/photo-1641982661987-351363e6785e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+  'https://images.unsplash.com/photo-1641982705401-1f0ed07ff5d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80',
+];
